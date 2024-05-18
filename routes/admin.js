7036a901cc51a06112fa64ec93bbee316d1b7a12
Suhail TH <suhailth17756@gmail.com>
2024-05-18 22:49:12 +0530

    serach in firbase added

--- routes/admin.js
+++ routes/admin.js
@@ -19,5 +19,6 @@ adminRouter.post("/companeylist",adminController.companyList)
 adminRouter.post("/addeditcompany",adminController.addeditcompany)
 adminRouter.post("/deletecompany",adminController.companyDelete)
 adminRouter.post("/getcompany",adminController.getCompany)
+adminRouter.post("/updatecompanystatus",adminController.companyStatus)
 
 module.exports=adminRouter
