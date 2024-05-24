b8c525f34e412b7ca2d74cda74c76c309e4e007b
Suhail TH <suhailth17756@gmail.com>
2024-05-24 21:49:08 +0530

    mutler upload has added

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@ const express=require("express")
 
 const adminRouter=express()
 const adminController=require("../controller/adminController")
-/*************************************************************************Admin routes****************************************************************************** */
+const { upload } = require("../middleware/multer");
+/****************************************************Admin routes */
 
 // adminRouter.post("/login",adminController.postLogin)
 adminRouter.post("/role_check",adminController.roleCheck)
 
-adminRouter.get("/get_users",adminController.getUsers)
 
 adminRouter.post("/getsubadmin",adminController.subAdminslist)
 adminRouter.post("/addsubadmin",adminController.createUpdateSubAdmin)
@@ -20,5 +20,14 @@ adminRouter.post("/addeditcompany",adminController.addeditcompany)
 adminRouter.post("/deletecompany",adminController.companyDelete)
 adminRouter.post("/getcompany",adminController.getCompany)
 adminRouter.post("/updatecompanystatus",adminController.companyStatus)
+adminRouter.get("/getcompnaynames",adminController.getcompanynames)
+
+
+adminRouter.post("/get_users",adminController.getUsersList)
+adminRouter.post("/addedituser",adminController.addedituser)
+adminRouter.post('/getuserdetail',adminController.getuserDetails)
+adminRouter.post("/updateuserstatus",adminController.userStatus)
+adminRouter.post("/users/delete",adminController.deleteUser)
+adminRouter.post('/bluckuploaduser', upload.single('file'),adminController.bulkuploaduser)
 
 module.exports=adminRouter
