e7c87bd4475acd2711f74405a414b2576c896cde
Suhail TH <suhailth17756@gmail.com>
2024-06-01 20:58:21 +0530

    origin added another

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const morgan=require("morgan")
 dbconnect.dbconnect()
 app.use(cors({
     credentials:true,
-    origin:['http://localhost:4200','http://localhost:4000','http://localhost:4201',"http://localhost:3700"]
+    origin:['http://localhost:4200','http://localhost:4000','http://localhost:4201',"http://localhost:3700","http://192.168.1.45:4200"]
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 app.use("/public/images",express.static(__dirname+"public/images"))  
 
-app.use(morgan("dev"))
+// app.use(morgan("dev"))
 app.use("/",adminRoutes)
 
 app.listen(4000,()=>{
