1d1125ee7891439ac6411e7ac924451db2ef3e50
Suhail TH <suhailth17756@gmail.com>
2024-06-13 08:33:08 +0530

    changes made in firebase config file

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,7 +1,6 @@
 const firebaseadmin = require("firebase-admin")
 const serviceAccount = require('../serviceAccountKey.json');
 
-
 let admin=firebaseadmin.initializeApp({
     credential: firebaseadmin.credential.cert(serviceAccount)
 })
