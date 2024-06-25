d7f133e3bb04c9aab229b35e63a086d82befe701
Suhail TH <suhailth17756@gmail.com>
2024-06-25 21:37:00 +0530

    profile update and get api changes made

--- controller/commonController.js
+++ controller/commonController.js
@@ -8,15 +8,20 @@ const { doc } = require("firebase/firestore");
 const profileData=async(req,res)=>{
     try {
         let {id,type}=req.body
-        // console.log(req.body)
+        let responseData
+        let companycity
         let data=(await admin.firestore().collection("UserNode").doc(id).get()).data()
-        let companycity=(await admin.firestore().collection("UserNode").doc(data.companyid).get()).data().city
-        
-        let responseData={
-            profildata:data,
-            cityies:companycity
+        if(type==="Trainer Login"){
+
+             companycity=(await admin.firestore().collection("UserNode").doc(data.companyid).get()).data().city
+            responseData={
+               profildata:data,
+               cityies:companycity
+           }
+        }else{
+            console.log(data)
         }
-        console.log(responseData)
+        
         res.send({message:"profile data",status:true,data:data,cityies:companycity})
     } catch (error) {
         console.log(error)
@@ -29,8 +34,10 @@ const profileUpdate=async(req,res)=>{
         let {_id}=req.body
         console.log(req.body)
         let docdata=await admin.firestore().collection("UserNode").doc(_id)
+        // console.log((await docdata.get()).data())
         await docdata.update(req.body)
         let responseData= (await admin.firestore().collection('UserNode').doc(_id).get()).data()
+        // console.log(responseData)
         let accesss=responseData.access
         res.send({message:"updated successfully",status:true,data:responseData,access:accesss})
     } catch (error) {
